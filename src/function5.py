1ff164783c3604b880f1dad0f17c8cc0e5cb3462
szpalm <szolnokipalma@gmail.com>
2022-10-18 20:09:52 +0200

    5

--- src/function5.py
+++ src/function5.py
@@ -1,6 +1,7 @@
 def function5(input_string: str, input_int: int) -> str:
     input_string2=""
     for i in range(len(input_string)):
-        if i%input_int==0:
+        if (i+1)%input_int==0:
             input_string2+=input_string[i]
     return input_string2
+
