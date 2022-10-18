563920b5d40bd4652586541d90e2c63e6a4dce3a
szpalm <szolnokipalma@gmail.com>
2022-10-18 19:21:48 +0200

    els

--- src/function3.py
+++ src/function3.py
@@ -3,5 +3,10 @@ input_list2 = [12, 45, 40]
 start_of_the_course = [18, 0, 0]
 
 def function3(input_list1: list, input_list2: list, start_of_the_course: list):
-    """Write your code here"""
+    msp_list1=input_list1[0]*3600+input_list1[1]*60+input_list1[2]
+    msp_list2=input_list2[0]*3600+input_list2[1]*60+input_list2[2]
+    msp_start_of_the_course=start_of_the_course[0]*3600+start_of_the_course[1]*60+start_of_the_course[2]
+    elso=msp_start_of_the_course-msp_list1
+    masodik=msp_start_of_the_course-msp_list2
+    return(elso,masodik)
     pass
