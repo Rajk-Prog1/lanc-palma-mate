f1c4387ce3bfe7fa2547dbd49c084a65a1e8f998
szpalm <szolnokipalma@gmail.com>
2022-10-18 19:34:29 +0200

    4

--- src/function4.py
+++ src/function4.py
@@ -1,3 +1,8 @@
 def function4(input_int1: int, input_int2: int) -> int:
-    """Write your code here"""
-    pass
+    kisebbik=min(input_int1,input_int2)
+    for i in range(kisebbik):
+        if (input_int1%kisebbik==0 and input_int2%kisebbik==0):
+            lko=kisebbik
+        else:
+            kisebbik=kisebbik-1
+    return lko
