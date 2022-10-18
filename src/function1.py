6997478e9df5defd7bd1269246e3ae06c22fa650
szpalm <szolnokipalma@gmail.com>
2022-10-18 19:08:23 +0200

    e

--- src/function1.py
+++ src/function1.py
@@ -1,9 +1,12 @@
-input_string = open('input_string.txt').read()[:-1]
 import string
+input_string = open('input_string.txt').read()[:-1]
+
 def function1(input_string: str) -> str:
     alphabet=list(string.ascii_lowercase)
     for i in input_string:
         for x in alphabet:
             if i==x:
-                input_string.remove(i)
+                input_string=input_string.replace(i,"")
     return input_string
+
+function1("edf56")
