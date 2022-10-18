53b0619629043e4da8d68ed081728d26e74f4736
szpalm <szolnokipalma@gmail.com>
2022-10-18 18:59:45 +0200

    elso

--- src/function1.py
+++ src/function1.py
@@ -1,5 +1,9 @@
 input_string = open('input_string.txt').read()[:-1]
-
+import string
 def function1(input_string: str) -> str:
-    """Write your code here"""
-    pass
+    alphabet=list(string.ascii_lowercase)
+    for i in len(input_string):
+        for x in alphabet:
+            if i==x:
+                input_string.remove(i)
+    return input_string
