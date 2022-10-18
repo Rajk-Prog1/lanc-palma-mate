c553583ce755d6724919906364dcfaeb2e0ed287
szpalm <szolnokipalma@gmail.com>
2022-10-18 19:01:35 +0200

    elso

--- src/function1.py
+++ src/function1.py
@@ -2,7 +2,7 @@ input_string = open('input_string.txt').read()[:-1]
 import string
 def function1(input_string: str) -> str:
     alphabet=list(string.ascii_lowercase)
-    for i in len(input_string):
+    for i in input_string:
         for x in alphabet:
             if i==x:
                 input_string.remove(i)
